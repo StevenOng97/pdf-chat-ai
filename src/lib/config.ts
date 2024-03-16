d3c50b9c1744eaa7aa2859b696307d4b84ea98b3
Nguyen <giaanguyen221197@gmail.com>
2024-03-16 16:05:41 +0700

    Update

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -6,7 +6,6 @@ const envSchema = z.object({
   NEXT_PUBLIC_PINECONE_ENVIRONMENT: z.string().trim().min(1),
   NEXT_PUBLIC_PINECONE_INDEX_NAME: z.string().trim().min(1),
   NEXT_PUBLIC_PINECONE_NAME_SPACE: z.string().trim().min(1),
-  NEXT_PUBLIC_PDF_PATH: z.string().trim().min(1),
   NEXT_PUBLIC_INDEX_INIT_TIMEOUT: z.coerce.number().min(1),
 });
 
